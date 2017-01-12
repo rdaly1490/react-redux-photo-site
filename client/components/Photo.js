f1557cd18311ff6fa6320444131fdf67ab7f1aae
Rob <rdaly1490@gmail.com>
2017-01-12 17:11:51 -0600

    rendering photos

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router';
+import CSSTransitionGroup from 'react-addons-css-transition-group';
 
 export default class Photo extends React.Component {
 	render() {
+		const { post, i, comments } = this.props;
+		const commentCount = comments[post.code] ? comments[post.code].length : 0;
 		return (
 			<figure className="grid-figure">
-				blah
+				<div className="grid-photo-wrap">
+					<Link to={`/view/${post.code}`}>
+						<img src={post.display_src} alt={post.caption} className="grid-photo" />
+					</Link>
+				</div>
+				<CSSTransitionGroup transitionName="like"
+					transitionEnterTimeout={500}
+					transitionLeaveTimeout={500}>
+					<span key={post.likes} className="likes-heart">{post.likes}</span>
+				</CSSTransitionGroup>
+				<figcaption>
+					<p>{post.caption}</p>
+					<div className="control-buttons">
+						<button className="likes">&hearts; {post.likes}</button>
+						<Link className="button" to={`/view/${post.code}`}>
+							<span className="comment-count">
+								<span className="speech-bubble"></span>
+								{` ${commentCount}`}
+							</span>
+						</Link>
+					</div>
+				</figcaption>
 			</figure>
 		);
 	}
--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -6,7 +6,7 @@ export default class PhotoGrid extends React.Component {
 		const { posts } = this.props;
 		return (
 			<div className="photo-grid">
-				{posts.map((post, index) => <Photo key={index} />)}
+				{posts.map((post, index) => <Photo {...this.props} key={index} i={index} post={post} />)}
 			</div>
 		);
 	}
