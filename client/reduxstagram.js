d4cff9a610ee602044b2e24e309115fac3b42042
Rob <rdaly1490@gmail.com>
2017-01-12 14:38:33 -0600

    init commit

---
+++ client/reduxstagram.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+// Import CSS
+import css from './styles/style.styl';
+
+render(<p>Hello World</p>, document.getElementById('root'));
+
+
+
+
